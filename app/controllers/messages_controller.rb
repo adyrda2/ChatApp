56e71d4b592536c6d01e41d2c5d72a18789b6ec8
adyrda2 <adyrda2@gmail.com>
2017-09-04 21:00:36 -0500

    initial commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,14 @@
+class MessagesController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+
+  def receives_data
+	name = params['name']
+  	message = params['message']
+  	puts name
+  	puts message
+  	Pusher.trigger('my-channel', 'my-event', {
+      message: message, name: name
+    })
+    render "message"
+  end
+end
