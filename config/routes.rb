86d73c3889e064e9252718cbcb8f7ea472acc516
adyrda2 <adyrda2@gmail.com>
2017-09-04 21:56:55 -0500

    Add message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'request' => 'messages#receives_data'
-
+  resource :messages do
+  	post 'request' => 'messages#receives_data'
+  end
 end
